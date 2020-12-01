fd98084d87eb77d0f38bb7120f7175d0f052383b
Lento Manickathan <Lento.manickathan@gmail.com>
2020-12-01 19:38:07 +0100

    first commit, added structure

---
+++ day1/main.cpp
@@ -0,0 +1,14 @@
+// Advent of Code: Day 1
+// Lento Manickathan
+
+#include <iostream>
+
+int main()
+{
+    unsigned int day;
+    std::cin >> day;
+    //const unsigned int day = 1;
+    std::cout << "Day: " << day << "\n";
+    //int* arr[]
+}
+
---
+++ day1/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main(filename='list.txt'):
+    arr = np.loadtxt(filename, dtype='int')
+    answer = np.prod(arr[np.argwhere((arr + arr[:,None]) == 2020)[0]])
+    print('answer:', answer)
+
+if __name__ == '__main__':
+    main()

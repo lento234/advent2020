8c9f1c8cbdb4f004f431cd811d8180e9f3a7e169
Lento Manickathan <Lento.manickathan@gmail.com>
2020-12-01 20:45:18 +0100

    full structure done

--- day1/main.cpp
+++ day1/main.cpp
@@ -5,16 +5,17 @@
 #include <fstream>
 #include <vector>
 
-std::vector<int> load_vector(std::string filename)
+template<typename T>
+std::vector<T> load_vector(std::string filename)
 {
-    std::vector<int> vec;
+    std::vector<T> vec;
     std::ifstream file(filename); // Input file stream object
    
     // Check if exists and then open the file.
     if (file.good())
     {
         // Push items into a vector
-        int value;
+        T value;
         while (file >> value)
             vec.push_back(value);
     }
@@ -28,30 +29,20 @@ int main()
 {
     // Info
     const unsigned int day = 1;
-    std::cout << "Day: " << day << "\n";
+    std::cout << "Advent of Code : Day " << day << "\n";
     
     // Read file
-    /*
-    std::vector<int> numbers;
-    std::ifstream file("numbers.txt"); // Input file stream object
-   
-    // Check if exists and then open the file.
-    if (file.good())
-    {
-        // Push items into a vector
-        int value;
-        while (file >> value)
-            numbers.push_back(value);
-    }
-    // Close the file.
-    file.close();
-    */
-    std::vector<int> numbers = load_vector("numbers.txt");
+    std::vector<int> numbers = load_vector<int>("numbers.txt");
 
     // Print array
-    std::cout << "array: ";
-    for (auto& i: numbers)
-        std::cout << i << " ";
-    std::cout << "\n";
+    std::cout << "Solution:\n";
+    for (auto& a: numbers)
+        for (auto& b: numbers)
+            if (a+b == 2020)
+            {
+                std::cout << a << " + " << b << " = " << a + b << "\n"
+                          << a << " * " << b << " = " << a * b << "\n";
+                return 0;
+            }
 
 }

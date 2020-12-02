e1b6898ce21dc76ec174a66651941c057e03b5c7
Lento Manickathan <Lento.manickathan@gmail.com>
2020-12-02 21:08:25 +0100

    setup fileread

--- day2/main.cpp
+++ day2/main.cpp
@@ -3,7 +3,28 @@
 
 #include <fstream>
 #include <vector>
+#include <string>
 #include <fmt/ranges.h>
+
+inline std::vector<std::string> read_file(const char* filename)
+{
+    std::vector<std::string> vec;
+    std::ifstream file(filename); // Input file stream object
+
+    // Check if exists and then open the file.
+    if (file.good())
+    {
+        // Push items into a vector
+        std::string value;
+        while (file >> value)
+            vec.push_back(value);
+    }
+    // Close the file.
+    file.close();
+
+    return vec;
+}
+
 
 int main()
 {
@@ -11,6 +32,7 @@ int main()
     const unsigned int day = 2;
     fmt::print("Advent of Code : Day {}\n", day);
    
-    std::vector<int> v = {1, 2, 3};
-    fmt::print("{}\n", v);
+    std::vector<std::string> input = read_file("test_input.txt");
+
+    fmt::print("{}\n", input);
 }

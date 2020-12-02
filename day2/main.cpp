16e274bc2e12254829f181e3e0940cd751310a48
Mrlento234 <lento.manickathan@gmail.com>
2020-12-02 19:51:44 +0100

    working on day2

---
+++ day2/main.cpp
@@ -0,0 +1,16 @@
+// Advent of Code: Day 2
+// Lento Manickathan
+
+#include <fstream>
+#include <vector>
+#include <fmt/ranges.h>
+
+int main()
+{
+    // Info
+    const unsigned int day = 2;
+    fmt::print("Advent of Code : Day {}\n", day);
+   
+    std::vector<int> v = {1, 2, 3};
+    fmt::print("{}\n", v);
+}

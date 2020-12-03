b28c0f86f5fd509b523a888b338a89ce4370aae1
Lento Manickathan <Lento.manickathan@gmail.com>
2020-12-03 15:02:08 +0100

    updated

--- day3/main.cpp
+++ day3/main.cpp
@@ -8,21 +8,6 @@
 #include <fmt/ranges.h>
 #include "util.h"
 
-//std::vector<uint32_t> 
-static void decode_input(const std::vector<std::string> input)
-{
-    size_t N = input.size();
-    size_t M = input[0].size();
-
-    // Decode message
-    bool message[N][M];
-    for (size_t i=0; i<N; ++i)
-        for (size_t j=0; j<M; ++j)
-            message[i][j] = (input[i][j] == '.') ? false : true;
-
-    fmt::print("message: {}", message[0][0]);
-}
-
 int main()
 {
     // Info
@@ -30,11 +15,7 @@ int main()
     fmt::print("Advent of Code : Day {}\n", day);
 
     // Read file
-    auto test_input = read_file("test_input.txt");
+    Message test_input("test_input.txt");
 
-    // Print content
-    fmt::print("{}\n", fmt::join(test_input, "\n"));
-
-    decode_input(test_input);
-
+    
 }
--- day3/util.h
+++ day3/util.h
@@ -4,25 +4,47 @@
 #include <vector>
 #include <fstream>
 
-
-inline std::vector<std::string> read_file(const std::string& filename)
+struct Message
 {
-    // Read file
-    std::ifstream file(filename);
-    if (!file.good())
-        throw std::runtime_error("Failed to open!");
+    std::vector<std::string> content;
+    size_t n_rows;
+    size_t n_cols;
+    bool decoded_message[n_cols];
 
-    // Buffer file
-    std::string buffer;
-    std::vector<std::string> file_content;
-    while (file >> buffer)
-        file_content.push_back(buffer);
+    //inline std::vector<std::string> read_file(const std::string& filename)
+    Message(const std::string& filename)
+    {
+        // Read file
+        std::ifstream file(filename);
+        if (!file.good())
+            throw std::runtime_error("Failed to open!");
+
+        // Buffer file
+        std::string buffer;
+        while (file >> buffer)
+            content.push_back(buffer);
+        
+        // Close the file.
+       
+        // Determine message
+        file.close();
+        n_rows = content.size();
+        n_cols = content[0].size();
+
+        // Decode message
+        decode();
+
+    }
+
+    inline size_t size() { return n_rows * n_cols; }
     
-    // Close the file.
-    file.close();
+    void decode()
+    {
+        //for (size_t i=0; i<n_rows; ++i)
+            for (size_t j=0; j<n_cols; ++j)
+                decoded_message[j] = (content[0][j] == 'x') ? true : false;
+    } 
 
-    return file_content;
-}
 
-
+};
 

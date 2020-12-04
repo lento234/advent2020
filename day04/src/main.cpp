bdc2275ee20f32ba9925a3dc77d05cc9b031a29f
Mrlento234 <lento.manickathan@gmail.com>
2020-12-04 09:01:30 +0100

    day 4 template added

---
+++ day04/src/main.cpp
@@ -0,0 +1,16 @@
+// Advent of Code: Day 4
+// Lento Manickathan
+#include <vector>
+#include <string>
+#include <list>
+#include <fmt/ranges.h>
+
+int main()
+{
+    // Header info
+    constexpr int day = 4;
+    fmt::print("//////////////////////////////\n");
+    fmt::print("    Advent of Code : Day {}   \n", day);
+    fmt::print("//////////////////////////////\n\n");
+
+}

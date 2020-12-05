2808afc62703f59d3278603b4a028da0cde2a8e3
Lento Manickathan <Lento.manickathan@gmail.com>
2020-12-05 11:33:07 +0100

    skeleton added

---
+++ day05/src/main.cpp
@@ -0,0 +1,31 @@
+// Advent of Code: Day 5
+// Lento Manickathan
+#include <vector>
+#include <string>
+#include <fmt/ranges.h>
+#include "util.h"
+
+int main()
+{
+    // Header info
+    constexpr int day = 5;
+    fmt::print("//////////////////////////////\n");
+    fmt::print("    Advent of Code : Day {}   \n", day);
+    fmt::print("//////////////////////////////\n\n");
+   
+    // Test input
+    Text test_input("test_input.txt");
+    test_input.print();
+
+    // Test Problem 1:
+    //uint32_t test_answer1 = problem1(test_people);
+    //fmt::print(">> Test 1: {}\n", (test_answer1 == 2) ? "PASSED" : "FAILED");
+    //uint32_t test_answer2 = problem2(test_people);
+    //fmt::print(">> Test 2: {}\n", (test_answer2 == 2) ? "PASSED" : "FAILED");
+
+    // Solve problem
+    //Text input("input.txt");
+    //fmt::print(">> Answer 1: {}\n", problem1());
+    //fmt::print(">> Answer 2: {}\n", problem2()); 
+
+}
---
+++ day05/src/util.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <fmt/ranges.h>
+
+struct Text
+{
+    std::vector<std::string> raw;
+
+    Text(const std::string& filename)
+    {
+        // Read file
+        std::ifstream file(filename);
+        if (!file.good())
+            throw std::runtime_error("Failed to open!");
+
+        // Buffer file
+        std::string buffer;
+        while (getline(file, buffer))
+            raw.push_back(buffer);
+        file.close();
+
+    }
+
+    size_t size() { return raw.size(); }
+
+    void print()
+    {
+        fmt::print("    {}\n\n", fmt::join(raw, "\n    "));
+    }
+
+};
+
